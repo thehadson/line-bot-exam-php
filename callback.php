cc12ac7828e2dc4ceeeba03d0c4634830e5f9218
thehadson <hadson_sm@hotmail.com>
2019-07-02 16:45:09 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -37,7 +37,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "$arrPostData";
+  $arrPostData['messages'][0]['text'] = "json_encode($arrPostData)";
 }else{
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
